c2128f937482b9984a07525980fbc627c62e21b6
Jasper Mason Catley <jasper.catley@students.mq.edu.au>
2024-06-04 08:39:32 +1000

    New files, stream attempt

---
+++ src/TR160i-Stream.py
@@ -0,0 +1,49 @@
+import ffmpeg
+import cv2
+import subprocess
+
+video_format = "avi"
+server_url = "http://localhost:8000"
+
+
+
+
+def start_streaming(width, height,fps):
+    process = (
+        ffmpeg
+        .input('pipe:', format='rawvideo',codec="rawvideo", pix_fmt='yuyv422', s='{}x{}'.format(width, height))
+        .output(
+            server_url + '/stream',
+            #codec = "copy", # use same codecs of the original video
+            listen=1, # enables HTTP server
+            pix_fmt="yuyv422",
+            preset="ultrafast",
+            f=video_format
+        )
+        .overwrite_output()
+        .run_async(pipe_stdin=True)
+    )
+    return process
+
+def init_cap():
+    cap = cv2.VideoCapture(1)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    return cap, width, height
+
+def run():
+    cap, width, height = init_cap()
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    streaming_process = start_streaming(width, height,fps)
+    while True:
+        ret, frame = cap.read()
+        if ret:
+            streaming_process.stdin.write(frame.tobytes())
+        else:
+            break
+    streaming_process.stdin.close()
+    streaming_process.wait()
+    cap.release()
+
+if __name__ == "__main__":
+    run()
